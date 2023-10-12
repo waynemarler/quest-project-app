ea11ddc2286a177d2e6aa0e0b26625f73556635b
waynemarler <waynemarler49@googlemail.com>
2023-10-12 14:32:42 +0100

    landing

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,18 +1,32 @@
 // landing.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import './landing.css';
 import logo from '../../assets/images/qg-logo.png';
 
 function Landing({ onEnter }){
+  const [animationEnded, setAnimationEnded] = useState(false);
+  
+  console.log('Animation Ended State:', animationEnded);
+
   return (
     <section className="landing-section">
-      <img src={logo} className="app-logo-landing" alt="logo" />
+      <img src={logo} className="app-logo-landing" alt="logo"  onAnimationEnd={() => {
+          console.log('Animation ended');  // This will log when the animation ends
+          setAnimationEnded(true);
+        }} />
       <h2 className="landing-subtitle">Innovative Betting Solutions</h2>
-      <button className="enter-button" onClick={onEnter}>Enter</button>
+      {animationEnded && <button className="enter-button" onClick={onEnter}>Enter</button>}
 
     </section>
+    
   );
+
+
+  
 }
+
+
+
 
 export default Landing;
